d83e8334fbafede49650c6506314cb512c8c513a
simmihugs <simon.graetz@gmx.de>
2026-02-09 15:40:23 +0100

    guessing game complete with random number and infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+use rand::RngExt;
 use std::io;
 
 fn main() {
-    print!("Enter text: ");
-
     let mut buf = String::new();
+    let secret_number = rand::rng().random_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut buf)
-        .expect("failed to read line");
+    loop {
+        println!("Enter your guess between 0 and 100: ");
 
-    println!("You entered: {}", buf);
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("failed to read line");
+
+        let guess = match buf.trim().parse() {
+            Ok(i) => i,
+            Err(_) => 42,
+        };
+        buf.clear();
+
+        if guess == secret_number {
+            println!("You guessed the secret number!");
+            break;
+        } else {
+            println!("You guess `{guess}` does not match the secret number!");
+        }
+    }
 }
