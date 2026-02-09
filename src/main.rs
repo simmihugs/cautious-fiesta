d18f3472ba5c4b6d548643f3434e0d66b0887171
simmihugs <simon.graetz@gmx.de>
2026-02-09 19:03:50 +0100

    array and panic!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 fn main() {
-    // let guess: i32 = " 42 ".trim().parse().expect("could not parse");
-    // println!("guess: {guess}");
+    let arr = [0, 1, 2, 3, 4, 5];
 
-    // let tup: (&str, &str, i32) = ("hello", "world!", 42);
-    // println!("{0}", tup.0);
+    println!("Value at which index?");
 
-    // let (x, y, _) = tup;
-    // println!("x: {x}\ty: {y}");
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).expect("test");
+    let index: usize = match buf.trim().parse() {
+        Ok(i) => i,
+        Err(_) => 42,
+    };
 
-    // let arr = [1, 2, 3];
-    // arr.iter().for_each(|x| println!("{}", x));
-
-    // ['a', 'b', 'c']
-    //     .iter()
-
-    //     .for_each(|(i, y)| print!("{y} "));
-    // println!("");
-
-    let b = [0; 13];
-    b.iter()
-        .enumerate()
-        .for_each(|(i, y)| println!("{i:3}: {y:3}"));
+    if index < arr.len() {
+        println!("{}", arr[index])
+    } else {
+        let arr_str = arr
+            .iter()
+            .map(|x| x.to_string())
+            .fold(String::new(), |mut arr, init| {
+                arr = arr + " " + init.as_str();
+                arr
+            });
+        panic!("index {index} is out of bound for array {arr_str}");
+    }
 }
