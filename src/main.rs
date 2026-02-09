34145ef802ae6668a5dc2ecbe7afa69ebed7e7c4
simmihugs <simon.graetz@gmx.de>
2026-02-09 15:16:34 +0100

    started guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter text: ");
+
+    let mut buf = String::new();
+
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("failed to read line");
+
+    println!("You entered: {}", buf);
 }
