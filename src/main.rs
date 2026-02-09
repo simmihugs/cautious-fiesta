4a9e306499d472d721e9e9d02c002c53522fcfb0
simmihugs <simon.graetz@gmx.de>
2026-02-09 16:04:59 +0100

    played around with constants and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,18 @@
-use rand::RngExt;
-use std::{cmp::Ordering, io};
+const HELLO_WORLD: &str = "hello, world!";
+const NUMBER: i32 = 42 * 1;
 
 fn main() {
-    let mut buf = String::new();
-    let secret_number = rand::rng().random_range(1..=100);
+    println!("{}, {NUMBER}", HELLO_WORLD);
 
-    loop {
-        println!("Enter your guess between 0 and 100: ");
-
-        io::stdin()
-            .read_line(&mut buf)
-            .expect("failed to read line");
-
-        let guess = match buf.trim().parse() {
-            Ok(i) => i,
-            Err(_) => 42,
-        };
-        buf.clear();
-
-        if guess == 42 {
-            println!("Aaaah, the answer to the universe, all and everything! Not the secret number though!");
-            continue;
-        }
-
-        match secret_number.cmp(&guess) {
-            Ordering::Greater => {
-                println!("You guess `{guess}` is smaller then the secret number {secret_number}!")
-            }
-            Ordering::Less => {
-                println!("You guess `{guess}` is greater then the secret number {secret_number}!")
-            }
-            Ordering::Equal => {
-                println!("You guessed the secret number!");
-                break;
-            }
-        };
+    let x = 42;
+    println!("x: {x}");
+    let x = x + 42;
+    println!("x: {x}");
+    {
+        let x = 15;
+        println!("x: {x}");
+        let x = "   ".len();
+        println!("x: {x}");
     }
+    println!("x: {x}");
 }
