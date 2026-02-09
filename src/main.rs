0b50a23fb252e1d01b073f173ca128f7684d415a
simmihugs <simon.graetz@gmx.de>
2026-02-09 16:33:14 +0100

    played around types, especially arrays

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-const HELLO_WORLD: &str = "hello, world!";
-const NUMBER: i32 = 42 * 1;
-
 fn main() {
-    println!("{}, {NUMBER}", HELLO_WORLD);
+    // let guess: i32 = " 42 ".trim().parse().expect("could not parse");
+    // println!("guess: {guess}");
 
-    let x = 42;
-    println!("x: {x}");
-    let x = x + 42;
-    println!("x: {x}");
-    {
-        let x = 15;
-        println!("x: {x}");
-        let x = "   ".len();
-        println!("x: {x}");
-    }
-    println!("x: {x}");
+    // let tup: (&str, &str, i32) = ("hello", "world!", 42);
+    // println!("{0}", tup.0);
+
+    // let (x, y, _) = tup;
+    // println!("x: {x}\ty: {y}");
+
+    // let arr = [1, 2, 3];
+    // arr.iter().for_each(|x| println!("{}", x));
+
+    // ['a', 'b', 'c']
+    //     .iter()
+
+    //     .for_each(|(i, y)| print!("{y} "));
+    // println!("");
+
+    let b = [0; 13];
+    b.iter()
+        .enumerate()
+        .for_each(|(i, y)| println!("{i:3}: {y:3}"));
 }
