cd7e22cdf64f6d1d4fd34956362b95585629a1c0
simmihugs <simon.graetz@gmx.de>
2026-02-10 14:27:15 +0100

    ownership example string

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-fn fibonacci(nth: i32) -> i32 {
-    if nth == 0 {
-        0
-    } else if nth == 1 {
-        1
-    } else {
-        fibonacci(nth - 1) + fibonacci(nth - 2)
-    }
-}
-
 fn main() {
-    println!("{}", fibonacci(5));
+    let mut s = String::from("hello");
+    println!("{s}");
+    s.push_str(", world!");
+    println!("{s}");
 }
