0793e2dc6ece13abcba8b1c408d72d2e6b681cc4
simmihugs <simon.graetz@gmx.de>
2026-02-10 14:12:25 +0100

    build fibonacci series

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
+fn fibonacci(nth: i32) -> i32 {
+    if nth == 0 {
+        0
+    } else if nth == 1 {
+        1
+    } else {
+        fibonacci(nth - 1) + fibonacci(nth - 2)
+    }
+}
+
 fn main() {
-    let temperatures = (-100..100).step_by(5);
-    temperatures.for_each(|t| {
-        let f = celcius_to_farenheit(t as f64);
-        let d = farenheit_to_celcius(f);
-        let are_the_same = d == t as f64;
-
-        print!("the temperature is {t:4}°C and {f:4}°F.\t");
-
-        println!("c={t}°C -> f={f}°F -> d={d}°C => d == c ?: {are_the_same}");
-    });
-}
-
-#[allow(dead_code)]
-fn farenheit_to_celcius(temperature: f64) -> f64 {
-    (temperature - 32.0) * (5.0 / 9.0)
-}
-
-fn celcius_to_farenheit(temperature: f64) -> f64 {
-    temperature * (9.0 / 5.0) + 32.0
+    println!("{}", fibonacci(5));
 }
