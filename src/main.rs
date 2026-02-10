7ddc15b4564dfb865d2fc058265c9b35d6d2f454
simmihugs <simon.graetz@gmx.de>
2026-02-10 13:45:36 +0100

    there is a while loop

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 fn main() {
-    let mut count = 1;
-    'counting_up: loop {
-        let mut loop_var = 10;
+    let mut count = 0;
 
-        if count < 3 {
-            println!("loop: {count}");
-        }
-
-        loop {
-            if count == 3 {
-                break 'counting_up;
-            }
-
-            if loop_var == 0 {
-                break;
-            }
-            println!("\tloop_2: {loop_var}");
-            loop_var -= 1;
-        }
+    while count != 10 {
+        println!("{count}");
         count += 1;
+
+        if count % 2 == 0 {
+            break;
+        }
     }
 }
