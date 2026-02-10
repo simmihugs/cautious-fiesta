945586c2182dbabc1eb35c232988db87e554043b
simmihugs <simon.graetz@gmx.de>
2026-02-10 14:08:29 +0100

    build farenheit to celsius and vice versa translation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    (1..10).rev().for_each(|a| {
-        println!("{a}");
-    });
+    let temperatures = (-100..100).step_by(5);
+    temperatures.for_each(|t| {
+        let f = celcius_to_farenheit(t as f64);
+        let d = farenheit_to_celcius(f);
+        let are_the_same = d == t as f64;
 
-    (0..5).for_each(|x| {
-        println!("{x}");
+        print!("the temperature is {t:4}°C and {f:4}°F.\t");
+
+        println!("c={t}°C -> f={f}°F -> d={d}°C => d == c ?: {are_the_same}");
     });
+}
+
+#[allow(dead_code)]
+fn farenheit_to_celcius(temperature: f64) -> f64 {
+    (temperature - 32.0) * (5.0 / 9.0)
+}
+
+fn celcius_to_farenheit(temperature: f64) -> f64 {
+    temperature * (9.0 / 5.0) + 32.0
 }
