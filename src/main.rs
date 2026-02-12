ba54abab700c41ac59ce4e2bcaf19b489ec4f2cd
simmihugs <simon.graetz@gmx.de>
2026-02-12 23:37:01 +0100

    structs unit-like as state for a nuclear powerplant - sligth fixes

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,91 @@
-#[derive(Debug)]
-enum Mode {
-    Off,
-    Idle,
-    Critical,
-}
+struct Off;
+struct Idle;
+struct Critical;
 
-struct NuclearPowerPlant {
+struct NuclearPowerPlant<Mode> {
     power_generated: f64,
+
     #[allow(dead_code)]
     mode: Mode,
 }
 
-impl NuclearPowerPlant {
+impl NuclearPowerPlant<Critical> {
     fn melts(&self) {
-        match self.mode {
-            Mode::Critical => {
-                println!("OH NOOOOOOOOO, WE ARE HAVING A MELTDOWN!");
-            }
-            _ => {
-                println!("Cannot melt in the current state {:?}", self.mode);
-            }
-        }
+        println!("OH NOOOOOOOOO, WE ARE HAVING A MELTDOWN!");
     }
 
-    fn save_us_jesus(&mut self) {
-        match self.mode {
-            Mode::Critical => {
-                println!("Help us Jebus!");
-                self.mode = Mode::Idle;
-                self.shutdown();
-            }
-            Mode::Off => {
-                println!("Not running");
-            }
-            Mode::Idle => {
-                println!("Not in crisis");
-            }
-        }
-    }
+    fn save_us_jesus(&self) -> NuclearPowerPlant<Off> {
+        println!("Help us Jebus!");
 
+        let pp = NuclearPowerPlant {
+            power_generated: self.power_generated,
+
+            mode: Idle,
+        };
+
+        pp.shutdown()
+    }
+}
+
+impl NuclearPowerPlant<Off> {
     fn new() -> Self {
         println!("Powerplant build.");
+
         NuclearPowerPlant {
             power_generated: 0.0,
-            mode: Mode::Off,
+            mode: Off,
         }
     }
 
-    fn start(&mut self) {
-        match self.mode {
-            Mode::Off => {
-                println!("Powerplant started.");
-                self.mode = Mode::Idle;
-            }
-            _ => {
-                println!("Cannot start in this state: {:?}", self.mode);
-            }
+    fn start(self) -> NuclearPowerPlant<Idle> {
+        println!("Powerplant started.");
+
+        NuclearPowerPlant {
+            power_generated: self.power_generated,
+            mode: Idle,
         }
     }
+}
 
+impl NuclearPowerPlant<Idle> {
     fn produces_energy(&mut self) {
-        match self.mode {
-            Mode::Idle => {
-                self.power_generated += 10.0;
-                println!("Dumdidum energy generated...");
-            }
-            _ => {
-                println!(" {:?}", self.mode);
-            }
+        self.power_generated += 10.0;
+
+        println!("Dumdidum energy generated...");
+    }
+
+    fn shutdown(self) -> NuclearPowerPlant<Off> {
+        println!("Shuting the plant down...");
+
+        println!("This much energy produced: {}", self.power_generated);
+
+        NuclearPowerPlant {
+            power_generated: self.power_generated,
+
+            mode: Off,
         }
     }
 
-    fn shutdown(&mut self) {
-        match self.mode {
-            Mode::Idle => {
-                println!("Shuting the plant down...");
-                println!("This much energy produced: {}", self.power_generated);
-                self.mode = Mode::Off;
-            }
-            Mode::Off => {
-                println!("Cannot shutdown, already shutdown!");
-            }
-            Mode::Critical => {
-                println!("Cannot shutdown! EMERGENCY!!!!");
-            }
-        };
-    }
+    fn meltdown(self) -> NuclearPowerPlant<Critical> {
+        println!("Oh my god!!");
 
-    fn meltdown(&mut self) {
-        match self.mode {
-            Mode::Idle => {
-                println!("Oh my god!!");
-                self.mode = Mode::Critical;
-            }
-            Mode::Critical => {
-                println!("Already PANIC!!!");
-            }
-            Mode::Off => {
-                println!("Nothing to see here");
-            }
+        NuclearPowerPlant {
+            power_generated: self.power_generated,
+
+            mode: Critical,
         }
     }
 }
 
 fn main() {
-    let mut power_plant = NuclearPowerPlant::new();
-    power_plant.start();
+    let power_plant = NuclearPowerPlant::new();
+    let mut power_plant = power_plant.start();
+
     for _ in 0..10 {
         power_plant.produces_energy();
     }
-    power_plant.meltdown();
+
+    let power_plant = power_plant.meltdown();
     power_plant.melts();
     power_plant.save_us_jesus();
 }
