81b9b98b0692e7f17fc659ff25a715f064dc6979
simmihugs <simon.graetz@gmx.de>
2026-02-12 19:33:13 +0100

    structs creation via ..other_struct is again a borrow

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 struct User {
     name: String,
     age: u32,
+    number: String,
 }
 
 fn main() {
     let mut user: User = User {
         name: "Peter Johnson".to_string(),
         age: 42,
+        number: "kkkk".to_string(),
     };
 
     println!("{:?}", user);
@@ -16,9 +18,15 @@ fn main() {
 
     println!("{:?}: age: {}", user, user.age);
 
-    let user2 = User {
+    let mut user2 = User {
         name: "blablabla".to_string(),
         ..user
     };
+    println!("{:?}", user2);
+
+    user2.age = 33;
+    user2.number = String::from("hello there");
+
+    println!("{:?}", user);
     println!("{:?}", user2);
 }
