048a3a6b110796b90a10da10ff1e1304024e6237
simmihugs <simon.graetz@gmx.de>
2026-02-12 21:36:58 +0100

    structs tuples

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
 #[derive(Debug, Clone)]
-struct User {
-    name: String,
-    age: u32,
-    number: String,
-}
+struct RGB(u8, u8, u8);
 
 fn main() {
-    let mut user: User = User {
-        name: "Peter Johnson".to_string(),
-        age: 42,
-        number: "kkkk".to_string(),
-    };
-
-    println!("{:?}", user);
-
-    user.name = String::from("kkkkkk");
-
-    println!("{:?}: age: {}", user, user.age);
-
-    let mut user2 = User {
-        name: "blablabla".to_string(),
-        ..user.clone()
-    };
-    println!("{:?}", user2);
-
-    user2.age = 33;
-    user2.number = String::from("hello there");
-
-    println!("{:?}", user);
-    println!("{:?}", user2);
+    let rgb: RGB = RGB(255, 255, 255);
+    println!("{:?}", rgb);
+    println!("red: {0}\tgreen: {1}\tblue: {2}", rgb.0, rgb.1, rgb.2);
 }
