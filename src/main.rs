520972aaf41efb2a76b1e419d286f7a82f7b9398
simmihugs <simon.graetz@gmx.de>
2026-02-12 21:58:37 +0100

    structs tuples with impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 #[derive(Debug, Clone)]
 struct RGB(u8, u8, u8);
 
+impl RGB {
+    fn red(&self) -> u8 {
+        self.0
+    }
+    fn green(&self) -> u8 {
+        self.1
+    }
+    fn blue(&self) -> u8 {
+        self.2
+    }
+}
+
 fn main() {
     let rgb: RGB = RGB(255, 255, 255);
     println!("{:?}", rgb);
-    println!("red: {0}\tgreen: {1}\tblue: {2}", rgb.0, rgb.1, rgb.2);
+    println!(
+        "red: {0}\tgreen: {1}\tblue: {2}",
+        rgb.red(),
+        rgb.green(),
+        rgb.blue()
+    );
 }
