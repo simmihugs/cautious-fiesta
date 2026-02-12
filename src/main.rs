9075f8e74daf229fb8bdf5b769aa30aa03967f45
simmihugs <simon.graetz@gmx.de>
2026-02-12 17:10:26 +0100

    double mutable borrow is not allowed

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
 fn main() {
-    let mut v = (1..10).collect::<Vec<i32>>();
-    v.iter().for_each(|x| {
-        println!("{x}");
-    });
+    let mut s = String::from("hello");
 
-    update2(&mut v);
-    //update(&mut v);
+    let r1 = &mut s;
+    let r2 = &mut s;
 
-    println!("");
-    v.iter().for_each(|x| {
-        println!("{x}");
-    });
-}
-
-fn update(v: &mut Vec<i32>) {
-    v.retain(|x| *x % 2 == 0);
-}
-
-fn update3(v: &mut Vec<i32>) {
-    // O(N) because needs to shift everything into new place
-    v.remove(3);
-}
-
-fn update2(v: &mut Vec<i32>) {
-    // like remove, but faster, because last element gets put into place of replaced element
-    // so messes order up
-    v.swap_remove(3);
+    println!("{r1} {r2}");
 }
