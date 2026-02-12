d50474aa784ea49d85cec4199be34ec0f18f5eb8
simmihugs <simon.graetz@gmx.de>
2026-02-12 22:23:48 +0100

    structs unit-like

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 #[derive(Debug, Clone)]
-struct RGB(u8, u8, u8);
+struct Element;
 
-impl RGB {
-    fn red(&self) -> u8 {
-        self.0
-    }
-    fn green(&self) -> u8 {
-        self.1
-    }
-    fn blue(&self) -> u8 {
-        self.2
+trait Strategy {
+    fn fun(&self);
+}
+
+impl Strategy for Element {
+    fn fun(&self) {
+        println!("{}", "hello, world!");
     }
 }
 
 fn main() {
-    let rgb: RGB = RGB(255, 255, 255);
-    println!("{:?}", rgb);
-    println!(
-        "red: {0}\tgreen: {1}\tblue: {2}",
-        rgb.red(),
-        rgb.green(),
-        rgb.blue()
-    );
+    let element = Element;
+
+    element.fun();
 }
