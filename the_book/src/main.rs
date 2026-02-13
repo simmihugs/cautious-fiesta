82e1423bf368078f21b26cdcdfafdca1bcad83ac
simmihugs <simon.graetz@gmx.de>
2026-02-13 19:56:39 +0100

    vector collections

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -3,4 +3,11 @@ use kuchen::blechkuchen as kuchen;
 fn main() {
     kuchen::gelee();
     kuchen::torte::sahne();
+
+    println!("Manfred Meisel");
+
+    let v: Vec<i32> = (0..10).collect();
+    v.iter().for_each(|x| {
+        println!("{x}");
+    });
 }
