e781a0929fdff3724179cc3b2bd0666996a7a570
simmihugs <simon.graetz@gmx.de>
2026-02-13 19:59:33 +0100

    vector collections, get function

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,13 +1,12 @@
-use kuchen::blechkuchen as kuchen;
-
 fn main() {
-    kuchen::gelee();
-    kuchen::torte::sahne();
-
-    println!("Manfred Meisel");
-
     let v: Vec<i32> = (0..10).collect();
     v.iter().for_each(|x| {
         println!("{x}");
     });
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element"),
+    }
 }
