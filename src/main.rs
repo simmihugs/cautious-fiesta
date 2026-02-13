a9def8c73c857c5fe32312335c8b81af08c5afc2
simmihugs <simon.graetz@gmx.de>
2026-02-13 10:50:45 +0100

    simple enum with String in fields

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn new(width: u32, height: u32) -> Self {
-        Rectangle {
-            width: dbg!(width),
-            height: dbg!(height),
-        }
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+enum Ipaddr {
+    #[allow(dead_code)]
+    V4(String),
+    #[allow(dead_code)]
+    V6(String),
 }
 
 fn main() {
-    let rect = Rectangle::new(16, 16);
-    dbg!(&rect);
+    let v4 = Ipaddr::V4("127.0.0.1".to_string());
+    let v6 = Ipaddr::V6("::1".to_string());
 
-    let rect2 = Rectangle::new(8, 15);
-
-    println!(
-        "does {rect2:?} completely fit into {rect:?}? -> {}",
-        rect.can_hold(&rect2)
-    );
+    println!("v4: {v4:?}\tv6: {v6:?}");
 }
