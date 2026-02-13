e3cc79013dbb457e0f66bb84028b26f1659add36
simmihugs <simon.graetz@gmx.de>
2026-02-13 20:02:15 +0100

    vector collections, slice vs. get

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let v: Vec<i32> = (0..10).collect();
-    v.iter().for_each(|x| {
-        println!("{x}");
-    });
+    let v = vec![1, 2, 3, 4, 5];
 
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element"),
-    }
+    // let does_not_exit = &v[100];
+    // println!("{does_not_exit}");
+
+    let does_not_exit2 = v.get(100);
+    println!("{does_not_exit2:?}");
 }
