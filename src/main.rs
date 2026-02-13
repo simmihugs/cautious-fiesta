b0231e6e1af45851db188942c46dce777ecab83c
simmihugs <simon.graetz@gmx.de>
2026-02-13 13:21:40 +0100

    packages: init

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,1 @@
-fn value_it(coin: &Option<i32>) -> Option<String> {
-    let Some(value) = coin else {
-        return None;
-    };
-
-    if *value > 42 {
-        Some(format!("value: {value} is high!"))
-    } else {
-        Some(format!("value: {value} is not high!"))
-    }
-}
-fn main() {
-    let coin = Some(32);
-    let value = value_it(&coin);
-    println!("{value:?}");
-}
+fn main() {}
