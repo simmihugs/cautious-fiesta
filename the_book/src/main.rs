b9deaf57b3b4ea59db869333dcb5b9ce7c86540c
simmihugs <simon.graetz@gmx.de>
2026-02-13 20:08:57 +0100

    vector collections, iterate over containers

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
 
-    //let vv = v.get(0);
-
-    v.push(6);
-
-    println!("first: {:?}", v.get(0));
-    println!("last: {:?}", v.last());
+    for i in &v {
+        println!("{i}");
+    }
 }
