8502e0d89e2718e1bddef73ef223d59c5ba0f8da
simmihugs <simon.graetz@gmx.de>
2026-02-14 21:14:27 +0100

    main can return error as well

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,37 +1,8 @@
-use std::{
-    fs::{self, File},
-    io::{self, Read},
-};
+use std::error::Error;
+use std::fs::File;
 
-fn read_user_name_from_file(file_path: &str) -> Result<String, io::Error> {
-    let mut file = File::open(file_path)?;
-    let mut text = String::new();
-    file.read_to_string(&mut text)?;
-    Ok(text)
-}
+fn main() -> Result<(), Box<dyn Error>> {
+    let _ = File::open("hello.txt")?;
 
-fn read_user_name_from_file_easy(file_path: &str) -> Result<String, io::Error> {
-    fs::read_to_string(file_path)
-}
-
-fn last_char_of_first_line(text: &str) -> Option<char> {
-    text.lines().next()?.chars().last()
-}
-
-fn main() {
-    let file_path = String::from("hello.txt");
-
-    let _res = read_user_name_from_file(&file_path).unwrap_or_else(|error| {
-        println!("{error:?}");
-        String::new()
-    });
-    let _res = read_user_name_from_file_easy(&file_path).unwrap_or_else(|error| {
-        println!("{error:?}");
-        String::new()
-    });
-
-    match last_char_of_first_line("hello\nworld!") {
-        Some(c) => println!("{c}"),
-        None => println!("no char found"),
-    }
+    Ok(())
 }
