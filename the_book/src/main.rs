0cd62d1a415a13925a424155f8c9953d1f97936f
simmihugs <simon.graetz@gmx.de>
2026-02-14 20:51:23 +0100

    unwrap or else to reduce match arms

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,25 +1,15 @@
 use std::{fs::File, io::ErrorKind};
 
-fn open_file(file_name: &str) -> Result<File, std::io::Error> {
-    let res = File::open(file_name)?;
-
-    Ok(res)
-}
-
 fn main() {
     let file_path = String::from("hello.txt");
-    let file_result = open_file(&file_path);
 
-    match file_result {
-        Ok(_) => println!("it worked!"),
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => match File::create(&file_path) {
-                Ok(_) => println!("it worked to create file!"),
-                Err(e) => panic!("Problme creating the file: {e:?}"),
-            },
-            _ => {
-                panic!("Problem opening the file: {e:?}");
-            }
-        },
-    };
+    let _file = File::open(&file_path).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create(&file_path).unwrap_or_else(|error| {
+                panic!("Problem creating the file: {error:?}");
+            })
+        } else {
+            panic!("Problem opening the file: {error:?}");
+        }
+    });
 }
