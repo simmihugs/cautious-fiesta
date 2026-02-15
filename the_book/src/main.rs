42f6fe5d1643fff4eb17e00fd9605e7867497823
simmihugs <simon.graetz@gmx.de>
2026-02-15 14:46:16 +0100

    struct based on 2 types

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,20 +1,11 @@
-fn find_largest<T: std::cmp::PartialOrd<T>>(vec: &[T]) -> &T {
-    let mut largest = &vec[0];
-    for v in vec {
-        if *v > *largest {
-            largest = v;
-        }
-    }
-
-    largest
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    let vec: Vec<i32> = (0..10).collect();
-    let largest = find_largest(&vec);
-    println!("for vec {vec:?} the largest element is: {largest}\n\n");
-
-    let vec: Vec<char> = ('a'..='z').collect();
-    let largest = find_largest(&vec);
-    println!("for vec {vec:?} the largest element is: {largest}\n\n");
+    let point: Point<i32, f64> = Point { x: 4, y: 5.4 };
+    println!("The point: {point:?}");
 }
