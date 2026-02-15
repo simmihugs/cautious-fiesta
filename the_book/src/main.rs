2c7b99c7c1936241392a221e42e8b40f8379e82e
simmihugs <simon.graetz@gmx.de>
2026-02-15 14:51:28 +0100

    impl on generic types can be based on the type! so different impl for different generic types are possible

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -5,7 +5,18 @@ struct Point<T, U> {
     y: U,
 }
 
+impl Point<f64, f64> {
+    fn wow(&self) -> Point<i32, i32> {
+        Point {
+            x: self.x.clone() as i32,
+            y: self.y.clone() as i32,
+        }
+    }
+}
+
 fn main() {
     let point: Point<i32, f64> = Point { x: 4, y: 5.4 };
     println!("The point: {point:?}");
+
+    println!("point: {:?}", (Point { x: 4.5, y: 8.9 }).wow());
 }
