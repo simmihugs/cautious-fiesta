328b037df21c49fc212910378f3e0f6f4d539464
simmihugs <simon.graetz@gmx.de>
2026-02-15 22:19:30 +0100

    IO project

---
+++ ioproject/target/debug/deps/ioproject-f788ca43cd18ace6.d
@@ -0,0 +1,5 @@
+/home/simmi/Projects/rustbook/ioproject/target/debug/deps/ioproject-f788ca43cd18ace6.d: src/main.rs
+
+/home/simmi/Projects/rustbook/ioproject/target/debug/deps/ioproject-f788ca43cd18ace6: src/main.rs
+
+src/main.rs:
---
+++ ioproject/target/debug/ioproject.d
@@ -0,0 +1,1 @@
+/home/simmi/Projects/rustbook/ioproject/target/debug/ioproject: /home/simmi/Projects/rustbook/ioproject/src/main.rs
