384ede57aff682a6ff7d65f5294f0929f129be62
simmihugs <simon.graetz@gmx.de>
2026-02-15 15:39:53 +0100

    enforce trait in methods by using &impl <Trait> in parameters list

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -2,12 +2,12 @@ trait Summary {
     fn summarize(&self) -> String;
 }
 
-fn write_out_summaries<T: Summary>(t: &T) {
-    println!("{:?}", t.summarize());
-}
-
 struct Article {
     text: String,
+}
+
+fn summarize_it(t: &impl Summary) {
+    println!("{}", t.summarize());
 }
 
 #[allow(dead_code)]
@@ -26,18 +26,17 @@ impl Article {
 
 impl Summary for Article {
     fn summarize(&self) -> String {
-        self.text.to_lowercase()[..50].to_string() + "..."
+        let tmp = self.text.to_lowercase();
+        if tmp.len() > 50 {
+            tmp[..50].to_string() + "..."
+        } else {
+            tmp[..].to_string() + "..."
+        }
     }
 }
 
 fn main() {
     let article = Article::from("Once upon a time there was a little shad. He was a bad bad bunny, and very insecure, which was a shame because he was a grifter as well.");
-    println!("article summary: {}", article.summarize());
 
-    write_out_summaries(&article);
-
-    let stories: Vec<Box<dyn Summary>> = vec![Box::new(article)];
-    stories.iter().for_each(|s| {
-        write_out_summaries(s.as_ref());
-    });
+    summarize_it(&article);
 }
