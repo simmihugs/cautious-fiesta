60ea8c4a89982cd66e6479076fa64d825b3fc6fc
simmihugs <simon.graetz@gmx.de>
2026-02-15 22:22:16 +0100

    moved ioproject

--- ioproject/src/main.rs
+++ ioproject/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    //dbg!(&args);
+
+    let search_string = args.get(1).ok_or("Missing search string")?;
+    let file_path = args.get(2).ok_or("Missing file")?;
+    let content = read_file(&file_path)?;
+
+    println!("searchstring: {search_string:?}");
+    println!("filepath: {file_path:?}");
+    println!("content: {content:?}");
+
+    Ok(())
+}
+
+fn read_file(file_path: &str) -> Result<String, std::io::Error> {
+    let file = std::fs::read_to_string(&file_path)?;
+
+    Ok(file)
 }
