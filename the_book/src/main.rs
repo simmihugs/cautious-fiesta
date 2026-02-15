83a0239b5e8e339172304945aadeb4aa4c59c67b
simmihugs <simon.graetz@gmx.de>
2026-02-15 16:27:51 +0100

    lifetimes via the borrow checker

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,74 +1,9 @@
-trait Summary {
-    fn summarize(&self) -> String;
-}
-
-trait Hello {
-    fn hello(&self) {
-        println!("hello")
-    }
-}
-
-struct Article {
-    text: String,
-}
-
-fn summarize_it(t: &impl Summary) {
-    println!("{}", t.summarize());
-}
-
-fn summarize_it_further(t: &(impl Summary + Hello)) {
-    println!("{}", t.summarize());
-    t.hello();
-}
-
-fn summarize_it_further_<T>(t: &T)
-where
-    T: Summary + Hello,
-{
-    println!("{}", t.summarize());
-    t.hello();
-}
-
-fn create_article() -> impl Summary + Hello {
-    Article {
-        text: "hello".to_string(),
-    }
-}
-
-#[allow(dead_code)]
-impl Article {
-    fn new() -> Self {
-        Article {
-            text: String::new(),
-        }
-    }
-    fn from(text: &str) -> Self {
-        Article {
-            text: String::from(text),
-        }
-    }
-}
-
-#[allow(dead_code)]
-impl Hello for Article {}
-
-impl Summary for Article {
-    fn summarize(&self) -> String {
-        let tmp = self.text.to_lowercase();
-        if tmp.len() > 50 {
-            tmp[..50].to_string() + "..."
-        } else {
-            tmp[..].to_string() + "..."
-        }
-    }
-}
-
 fn main() {
-    let article = Article::from("Once upon a time there was a little shad. He was a bad bad bunny, and very insecure, which was a shame because he was a grifter as well.");
+    let r;
+    {
+        let x = 5;
+        r = &x;
+    }
 
-    summarize_it(&article);
-    summarize_it_further(&article);
-    summarize_it_further_(&article);
-
-    summarize_it_further_(&create_article());
+    println!("r: {r}");
 }
