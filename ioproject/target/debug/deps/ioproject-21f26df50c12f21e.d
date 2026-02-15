60afc9c342b27973728bce591432884ac71acb59
simmihugs <simon.graetz@gmx.de>
2026-02-15 22:23:47 +0100

    moved ioproject

--- ioproject/target/debug/deps/ioproject-21f26df50c12f21e.d
+++
@@ -1,5 +0,0 @@
-/home/simmi/Projects/rustbook/ioproject/target/debug/deps/ioproject-21f26df50c12f21e.d: src/main.rs
-
-/home/simmi/Projects/rustbook/ioproject/target/debug/deps/libioproject-21f26df50c12f21e.rmeta: src/main.rs
-
-src/main.rs:
--- ioproject/target/debug/deps/ioproject-4b1a1a933a2f70b3.d
+++
@@ -1,5 +0,0 @@
-/home/simmi/Projects/rustbook/ioproject/target/debug/deps/ioproject-4b1a1a933a2f70b3.d: src/main.rs
-
-/home/simmi/Projects/rustbook/ioproject/target/debug/deps/libioproject-4b1a1a933a2f70b3.rmeta: src/main.rs
-
-src/main.rs:
