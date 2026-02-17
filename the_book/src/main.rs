5b28f83c87773a3426c7b30498cc5778d39e0665
simmihugs <simon.graetz@gmx.de>
2026-02-17 15:40:18 +0100

    Implementing Drop on types

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,43 +1,18 @@
-use std::ops::{Deref, DerefMut};
-
-#[derive(Debug)]
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+#[allow(dead_code)]
+struct Haus {
+    a: i32,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Drop for Haus {
+    fn drop(&mut self) {
+        println!("So long and thanks for all the fish!");
     }
-}
-
-impl<T> DerefMut for MyBox<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {name}!");
 }
 
 fn main() {
-    let x = 42;
-    let mut my_box = MyBox::new(x);
-    println!("{my_box:?}");
-    *my_box = 0;
-    println!("{my_box:?}");
-    *my_box = 42;
-    println!("{my_box:?}");
-    assert_eq!(42, x);
-    assert_eq!(42, *my_box);
-
-    let name = MyBox::new("Stefan");
-    hello(&name);
+    {
+        let _haus = Haus { a: 42 };
+    }
+    let haus = Haus { a: 42 };
+    drop(haus);
 }
