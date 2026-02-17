493626f60e279a39613fc7e8da3e8379e2a238e2
simmihugs <simon.graetz@gmx.de>
2026-02-17 13:16:29 +0100

    created iterator impl

--- the_book/src/main.rs
+++ the_book/src/main.rs
@@ -1,7 +1,8 @@
 struct Silly {
-    a: Option<i32>,
-    b: Option<i32>,
-    c: Option<i32>,
+    // a: Option<i32>,
+    // b: Option<i32>,
+    // c: Option<i32>,
+    content: Vec<i32>,
 }
 
 struct SillyIter<'a> {
@@ -22,12 +23,19 @@ impl<'a> Iterator for SillyIter<'a> {
     type Item = &'a i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let result = match self.state {
-            0 => self.silly.a.as_ref(),
-            1 => self.silly.b.as_ref(),
-            2 => self.silly.c.as_ref(),
-            _ => None,
+        // let result = match self.state {
+        //     0 => self.silly.a.as_ref(),
+        //     1 => self.silly.b.as_ref(),
+        //     2 => self.silly.c.as_ref(),
+        //     _ => None,
+        // };
+
+        let result = if self.state < self.silly.content.len() {
+            Some(&self.silly.content[self.state])
+        } else {
+            None
         };
+
         self.state += 1;
         result
     }
@@ -35,9 +43,10 @@ impl<'a> Iterator for SillyIter<'a> {
 
 fn main() {
     let silly = Silly {
-        a: Some(42),
-        b: Some(42),
-        c: Some(42),
+        // a: Some(42),
+        // b: Some(42),
+        // c: Some(42),
+        content: (0..10).collect(),
     };
 
     silly.iter().for_each(|x| {
